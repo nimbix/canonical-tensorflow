f2369321dc66796ca3052af17899bffaa1a5d00d
Stephen Fox <stephen.fox@nimbix.net>
2017-04-19 17:14:31 -0500

    Add connect URL

--- tutorials/start_wrapper.sh
+++ tutorials/start_wrapper.sh
@@ -8,14 +8,15 @@ export TASK_TYPE="$1"
 export TASK_ID=$2
 
 if [ ! -r /data/tutorials/config.json ]; then 
-   export TF_CONFIG=$(python `dirname $0`/gen_config.py)
+   export TF_CONFIG=$(python /usr/local/distributed-tensorflow/tutorials/gen_config.py)
 else
-   export TF_CONFIG=$(cat `dirname $0`/config.json)
+   export TF_CONFIG=$(cat /data/config.json)
 fi
 
 echo "TF_CONFIG: $TF_CONFIG"
+
 if [ "$TASK_TYPE" = "master" ]; then
-  /data/tutorials/tensorflow.sh /data/tutorials/simple_dist_experiment.py train_and_evaluate >> /tmp/$$.log
+  /data/tutorials/tensorflow.sh /usr/local/distributed-tensorflow/tutorials/simple_dist_experiment.py train_and_evaluate >> /tmp/$$.log
 else
-  /data/tutorials/tensorflow.sh /data/tutorials/simple_dist_experiment.py train >> /tmp/$$.log
+  /data/tutorials/tensorflow.sh /usr/local/distributed-tensorflow/tutorials/simple_dist_experiment.py train >> /tmp/$$.log
 fi
--- tutorials/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-NODES=$(cat /etc/JARVICE/cores| wc -l)
-
-echo $(( ${NODES}*16 ))
-
