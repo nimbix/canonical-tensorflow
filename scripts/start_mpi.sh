b7d62a7513301fa2592f5b1244fe5ee57b43c83c
Stephen Fox <stephen.fox@nimbix.net>
2017-04-20 13:11:47 -0500

    commit start_mpi.sh

--- scripts/start_mpi.sh
+++ scripts/start_mpi.sh
@@ -2,6 +2,7 @@
 
 SCRIPT="$1"
 GPUS_PER_NODE=4
+TENSORFLOW_OUTDIR=/data/tensorflow-output
 
 export PYTHONPATH=$PYTHONPATH:/usr/local/distributed-tensorflow/tools/lib
 export PATH=$PATH:/usr/local/distributed-tensorflow/tools/bin
@@ -12,5 +13,11 @@ export PATH=$PATH:/usr/local/distributed-tensorflow/tools/bin
 export JOB_NAME
 export EXPERIMENT_ID
 export GPUS_PER_NODE
+
+mkdir -p ${TENSORFLOW_OUTDIR}
+cd ${TENSORFLOW_OUTDIR}
+tensorboard --logdir ${TENSORFLOW_OUTDIR} &
+
+sudo service ssh start 2>/dev/null
 
 `which mpirun` -x JOB_NAME,EXPERIMENT_ID -hostfile /etc/JARVICE/nodes --map-by ppr:${GPUS_PER_NODE}:node /usr/local/distributed-tensorflow/scripts/mpiwrapper.sh 
